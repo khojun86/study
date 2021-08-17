92198caf2826cdc3c5ccfb3b1b0ba37150d0d070
khojun86 <82401279+khojun86@users.noreply.github.com>
2021-08-17 18:05:59 +0900

    Update 0528.py

--- 0528.py
+++ 0528.py
@@ -2,6 +2,7 @@ from pyupbit import WebSocketManager, pd
 import pyupbit
 import time
 import websockets
+import telegram
 import asyncio
 import json
 import numpy as np
@@ -15,30 +16,30 @@ import pprint
 #        print(data)
 #    wm.terminate()
 
-def sectionRising(data1, data2, data3):  # old state 를 만들어야댐
-
-    if data1 < data2 < data3:
-        return "up"
-    elif data2 < data3:  # old state 를 참조해야댐
-        return "reverse"
-    else:
-        return "down"
-
-
-def iscrossed(old, new, benchmark):
-    if old <= benchmark < new:
-        return 'golden'
-    elif old >= benchmark > new:
-        return 'dead'
-    else:
-        return False
+# def sectionRising(data1, data2, data3):  # old state 를 만들어야댐
+#
+#     if data1 < data2 < data3:
+#         return "up"
+#     elif data2 < data3:  # old state 를 참조해야댐
+#         return "reverse"
+#     else:
+#         return "down"
+#
+#
+# def iscrossed(old, new, benchmark):
+#     if old <= benchmark < new:
+#         return 'golden'
+#     elif old >= benchmark > new:
+#         return 'dead'
+#     else:
+#         return False
 
 
 async def upbit_ws_client(ticker):
     url = "wss://api.upbit.com/websocket/v1"
-    prevTime = ''
+    prevTime = '';buyTime = time.strftime('%H:%M')
     tickprice = np.array([])
-    tickgroup = 30
+    # tickgroup = 30
     firstbuyprice = 5010
     buyprice = firstbuyprice;sellprice = 0
     boughtedbalance = 0;boughtedvolume = 0;boughtedprice = 0
@@ -47,6 +48,7 @@ async def upbit_ws_client(ticker):
     # 초기 설정
     init_balance = upbit.get_balance("KRW")
     init_volume = upbit.get_balance(ticker)
+    #### telegram alarm ####
     # print("init_balance =", init_balance, "\ninit_volume =", init_volume)
     ####  ohlcv 변동 폭 계산 ####
     df = pyupbit.get_ohlcv(ticker, interval="minute240", count=10)
@@ -61,7 +63,7 @@ async def upbit_ws_client(ticker):
     # pprint.pprint(df)
     # print(df['high'],df['low'],(df['high']-df['low'])/df['low'])
     dayrange=np.array((df['high']-df['low'])/df['low'])
-    margin = float(np.mean(range) / 3)+dayrange[-1] / 2
+    margin = float(np.mean(range) / 2.8)+dayrange[-1] / 1.5
     ## tic 판단
     close = nexthigh[-1]
     if close > 2000000: tic = 1000
@@ -76,17 +78,24 @@ async def upbit_ws_client(ticker):
     print('margin =', margin)
     bbl_last = 0; bbl=0
 
+    ## 초기 주문 있을 때 대응
+    init_order=upbit.get_order(ticker)
+    if len(init_order)!=0:
+        # print(init_order[0])
+        remains=upbit.get_balances()[1]
+        ordered_abp=remains['avg_buy_price']
+        ordered_volume=remains['locked']
+        ordered_totalbalance=float(ordered_abp)*float(ordered_volume)
+        # print(ordered_abp,ordered_volume,ordered_totalbalance)
+        upbit.cancel_order(init_order[0]['uuid'])
+        time.sleep(1)
+        boughtedbalance = ordered_totalbalance
+        boughtedvolume = ordered_volume
+
 
     async with websockets.connect(url) as websocket:
-        subscribe_fmt = [
-            {"ticket": "test00"},
-            {
-                "type": "trade",
-                "codes": [ticker],
-                "isOnlyRealtime": True
-            }
+        subscribe_fmt = [{"ticket": "test00"},{"type": "trade","codes": [ticker],"isOnlyRealtime": True}]
             # ,{"format":"SIMPLE"}
-        ]
         subscribe_data = json.dumps(subscribe_fmt)
         await websocket.send(subscribe_data)
         while True:
@@ -96,7 +105,8 @@ async def upbit_ws_client(ticker):
                 data = json.loads(data)
                 # print(data)
                 ##### 30 tic close : tickprice[tickgroup-1] ####
-                if len(tickprice) < tickgroup:
+                # if len(tickprice) < tickgroup:
+                if len(tickprice) < 30:
                     tickprice = np.append(tickprice, [data["trade_price"]])
                 ##### grouptickclose for ma caculation ####
                 else:
@@ -111,7 +121,7 @@ async def upbit_ws_client(ticker):
                 if nowTime != prevTime:
                     df = pyupbit.get_ohlcv(ticker, 'minute1')
                     df_close = df['close']
-                    df_volume = df['volume']
+                    # df_volume = df['volume']
 
                     #### minute candle moving average ####
                     # minute_ma5 = df_close.rolling(5).mean()
@@ -136,12 +146,19 @@ async def upbit_ws_client(ticker):
 
                     bbm = minute_ma20[199]
                     bbl = bbm - roll_ma20.std()[199] * 2.4
-                    bbu = bbm + roll_ma20.std()[199] * 2.4
+                    bbu = bbm + roll_ma20.std()[199] * 2.4 # print(round(bbl, 0), round(bbm, 0));print("BB 하단",round(bbl, 0))
+                    #### 0817 update 매도 가 이상 도달 시
+                    if sellprice !=0 and data["trade_price"]>sellprice:
+                        boughtedbalance = 0;boughtedvolume = 0;sellprice = 0;selluuid = ''
+                        init_balance = balance;init_volume = upbit.get_balance(ticker)
+                        print("매도확인 =\033[31m",init_balance, "\033[0m")
+                        #### telegram alarm ####
+                        bot.sendMessage(chat_id=tlgm_id, text='현재' + str(init_balance))
 
-                    # print(round(bbl, 0), round(bbm, 0));print("BB 하단",round(bbl, 0))
+                    prevTime = nowTime
 
-                #### 현재가가 BB low 아래에 있을 때 매수가 결정 ####
-                if data["trade_price"] < bbl and state == 'none' and balance >= buyprice and len(grouptickclose) == 65:
+                #### 현재가가 BB low 아래에 있을 때 매수 결정 ####
+                if data["trade_price"] < bbl and state == 'none' and balance >= buyprice and len(grouptickclose) == 65 and nowTime!=buyTime:
                     state = 'ready'
                     if balance != init_balance:
                         if abp > data["trade_price"]:
@@ -158,49 +175,59 @@ async def upbit_ws_client(ticker):
                     tick30_ma10last = grouptickclose[54:64].mean()
                     tick30_ma10 = grouptickclose[55:65].mean()#;print(state)
                     if tick30_ma10last <= tick30_ma50 < tick30_ma10 and np.isnan(tick30_ma50) == False:
-                        print('매수지점', data["trade_price"])
+                        print(time.strftime('%H:%M'),'매수시간')
                         buy=upbit.buy_market_order(ticker, buyprice)
                         uuid=buy['uuid']
-                        print(uuid,type(uuid))
+                        # print(uuid,type(uuid))
                         state = 'buy';print(state)
                         # buyprice += 1
+                        buyTime = time.strftime('%H:%M')
                 #### 매수 한 다음 1분 중 매도가 결정 매도주문 ####
                 if state == 'buy' and nowTime != prevTime:
                     state = 'none';print(state)
-                    boughtedbalance += buyprice
+                    boughtedbalance += upbit.get_order(ticker)
                     boughtedvolume += upbit.get_balance(ticker)-init_volume
                     abp = boughtedbalance/boughtedvolume
-                    print('누적 Balance =',boughtedbalance,'init Volume',init_volume,'누적 Volume =',round(boughtedvolume,2),'ABP =',round(abp,2))
-                    print('ABP+1%',round(abp*1.01,2),'ABP+2%',round(abp*1.02,2),'ABP+3%',round(abp*1.03,2))
+                    print('Bought Balance =',boughtedbalance,'Bought Volume =',round(boughtedvolume,2),'ABP =',round(abp,2))
+                    # print('ABP+1%',round(abp*1.01,2),'ABP+2%',round(abp*1.02,2),'ABP+3%',round(abp*1.03,2))
                           # ,'\n','매도주문')
                     if sellprice == 0:
                         sellprice = int(abp*(1+margin/100)/tic)*tic
                     else:
                         upbit.cancel_order(selluuid)
+                        time.sleep(2)
                         sellprice = int(abp*(1+margin/100)/tic)*tic
-
+                        # pprint.pprint(upbit.get_order(ticker))
+                        time.sleep(2)
+                    print(time.strftime('%H:%M'),"매도가 ABP+",margin,"% = ",sellprice)
                     sellorder = upbit.sell_limit_order(ticker, sellprice, boughtedvolume)
-                    time.sleep(1)
+                    time.sleep(2)
                     selluuid = sellorder['uuid']
-                    pprint.pprint(sellorder)
+                    # pprint.pprint(sellorder)
 
-
-                if nowTime != prevTime:
-                    #### 0526 update 매도 주문 체결 시 초기화 ####
-                    order = upbit.get_order(ticker, state="wait")
-                    sellstate = 0
-                    if len(order) != 0:
-                        for i in order:
-                            if i['uuid'] == selluuid:
-                                sellstate += 1
-                            else:
-                                sellstate += 0
-                        if sellstate == 0 and selluuid != '':
-                            boughtedbalance = 0;boughtedvolume = 0;sellprice = 0;selluuid=''
-                            init_balance = upbit.get_balance("KRW")
-                            init_volume = upbit.get_balance(ticker)
-                            print("매도체결\n","init_balance =", init_balance, "\ninit_volume =", init_volume)
-                    prevTime = nowTime
+                # if nowTime != prevTime:
+                #     #### 0526 update 매도 주문 체결 시 초기화 ####
+                #     # order = upbit.get_order(ticker, state="wait")
+                #     # sellstate = 0
+                #     # if len(order) != 0:
+                #     #     for i in order:
+                #     #         if i['uuid'] == selluuid:
+                #     #             sellstate += 1
+                #     #         else:
+                #     #             sellstate += 0
+                #     #     if sellstate == 0 and selluuid != '':
+                #     #         boughtedbalance = 0;boughtedvolume = 0;sellprice = 0;selluuid=''
+                #     #         init_balance = upbit.get_balance("KRW")
+                #     #         init_volume = upbit.get_balance(ticker)
+                #     #         print("매도체결\n","init_balance =", init_balance, "\ninit_volume =", init_volume)
+                #
+                #     #### 0817 update 매도 주문 힝 발생 시
+                #     # 연구필요
+                #
+                #     #### 0817 update 매도 가 이상 도달 시
+                #     if sellprice !=0 and data["trade_price"]>sellprice:
+                #         print("헹")
+                #     prevTime = nowTime
             except Exception as e:
                 print('힝')
 async def main(ticker):
@@ -213,6 +240,17 @@ secret_key = "TS2cSWkl0vUkwGgmxQmyCdgeLydxNBlYacbowHp5"
 
 upbit = pyupbit.Upbit(access_key, secret_key)
 # print("init_balance =",upbit.get_balance("KRW"),"\ninit_volume =",upbit.get_balance(ticker))
-print("init_balance =\033[31m",upbit.get_balance("KRW"),"\033[30m")
+balance=upbit.get_balance("KRW")
+print("init_balance =\033[31m",balance,"\033[0m")
+# pprint.pprint(upbit.get_order(ticker))
+
+# 텔레그램 연결
+tlgm_token = '1733326409:AAFVLFVWG2nPJcP65YcglWgQKeKLQtJuzE4'
+tlgm_id = '1758620562'
+bot = telegram.Bot(token = tlgm_token)
+updates = bot.getUpdates()
+bot.sendMessage(chat_id=tlgm_id, text='시작'+str(balance))
+# telegram_mid=bot.getUpdates()[-1].message.message_id
+
 
 asyncio.run(main(ticker))
